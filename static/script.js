45467646248f2c85cf6e4d14f2008ead0d548ba0
Roxann Huebsch <roxannhuebsch@outlook.com>
2024-06-30 11:22:56 +0000

    add more structure to base template, with styling, bugs still present

--- static/script.js
+++ static/script.js
@@ -1,13 +1,2 @@
-// nav bar functioning 
-function browseTerms(letter) {
-    // Logic to filter terms by selected letter and update word list
-    // You may use AJAX to fetch and update the word list dynamically
-    alert('Browse terms starting with: ' + letter);
-}
 
-function searchTerms() {
-    // Logic to filter terms by search input and update word list
-    // You may use AJAX to fetch and update the word list dynamically
-    let searchQuery = document.getElementById('search').value;
-    alert('Search for: ' + searchQuery);
-}
+
