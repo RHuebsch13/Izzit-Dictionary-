d179d9c2a4f707af684b651c469d8d918fa36ae0
Roxann Huebsch <roxannhuebsch@outlook.com>
2024-07-26 13:08:57 +0000

    debug=False

--- app.py
+++ app.py
@@ -243,4 +243,4 @@ def delete_term(term_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP", "0.0.0.0"),
             port=int(os.environ.get("PORT", 5000)),
-            debug=True)
+            debug=False)
